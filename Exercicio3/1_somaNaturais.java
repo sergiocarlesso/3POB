c097f89e0f92a51620919bcba58388a4feb1a6b6
sergiocarlesso <sergiocarlessof@hotmail.com>
2025-09-15 18:15:42 -0300

    Create 1_somaNaturais.java

---
+++ Exercicio3/1_somaNaturais.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SomaNumeros {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a quantidade de numeros a ser somado: ");
+        int n = sc.nextInt();
+
+        int somaNaturais = 0; // variável para somar
+        for (int i = 1; i <= n; i++) {
+            somaNaturais += i;
+        }
+
+        System.out.println("A soma dos numeros naturais é: " + somaNaturais);
+
+        sc.close();
+    }
+}
